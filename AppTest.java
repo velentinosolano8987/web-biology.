82ee779b2e250cb309865b3cc3147ebefcdfe725
velentinosolano8987 <vduttosolano020@materpurissima.edu.ar>
2025-02-15 11:37:30 -0300

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,15 @@
+Import java.beans.Transient;
+
+class AppTest {
+
+    void appHasAGreeting() {
+        
+    assertNotNull.Return(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+
+    @Override
+    public String toString() {
+        return "AppTest []";
+    }
+
+}

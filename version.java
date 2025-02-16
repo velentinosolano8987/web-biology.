0f05258ac767d118c3ac9e21348a20e22b62cd06
velentinosolano8987 <vduttosolano020@materpurissima.edu.ar>
2025-02-16 17:18:44 -0300

    Add files via upload

---
+++ version.java
@@ -0,0 +1,11 @@
+package jdk.internal.jshell.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "23.0.1+11-39" },
+            { "jdk", "23.0.1" },
+            { "release", "23.0.1" },
+        };
+    }
+}

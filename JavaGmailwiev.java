71cb93a10d25f3e7f50ebb4a9416bb768cbab0c6
velentinosolano8987 <vduttosolano020@materpurissima.edu.ar>
2025-05-03 20:31:49 -0300

    Update JavaGmailwiev.java

--- JavaGmailwiev.java
+++ JavaGmailwiev.java
@@ -1,29 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
-
-/**
- *
- * @author Usuario
- */
-public class JavaGmailwiev {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-       
-    }
-}
-
 package com.example.demo;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.org.springframework.steryotype.controller;
 import java.org.springframework.ui.Model;
 import java.org.springframework.web.bind.annotation.GetMapping;
 import java.org.springframework.web.bind.annotation.RequestParam;
 import java.org.springframework.web.bind.annotation.PostMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+public class JavaGmailwiev {
+
+    public static void main(String args[]) {
 
 @Controller
 public class EmailController{
@@ -35,18 +29,22 @@ public String ShowForm() {
 }
 
 @PostMapping("/Save-Email")
-Public string save.Email(@RequestParam("email")String email,Model Model{
+Public.string save_email(@RequestParam("email")String email; Model model]){
 
-if((email == null /!email.endsWith (@gmail.com ){
+if(email == null /!email.endsWith. gmail.com) {
 model.addAttribute("message","Invalid Gmail address.");
-return."index";)
+return "index";
 }
 
-try(FileWriter writer = new FileWriter("emails.txt",true))
+try (FileWriter writer = new FileWriter("emails.txt",true));
 write.write(email +"\n");
-model.addAttribute("message", "Email saved succesfuly!")
-}catch(IOException e){
+model.addAttribute("message", "Email saved succesfuly!");
+
+catch(IOException e){
 e.PrintStackTrace();
 model.addAttribute("message" , "Failed to save email.");
-Return "index";
-
+return "index";
+}
+}
+}
+}
